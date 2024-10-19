edab86f6f6566cebbc471307718a7e2db58540da
池田慎太郎 <ikeshi0015@icloud.com>
2024-10-20 01:11:43 +0900

    サイドバー修正

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import "./Sidebar.scss";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddIcon from "@mui/icons-material/Add";
+import SidebarChannel from "./SidebarChannel";
+import MicIcon from "@mui/icons-material/Mic";
+import HeadphonesIcon from "@mui/icons-material/Headphones";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 const Sidebar = () => {
   return (
@@ -29,6 +33,26 @@ const Sidebar = () => {
               <h4>テストチャンネル</h4>
             </div>
             <AddIcon className="sidebarAddIcon" />
+          </div>
+          <div className="sidebarChannelList">
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+          </div>
+          <div className="sidabarFooter">
+            <div className="sidebarAccount">
+              <img src="./icon.png" alt=""></img>
+              <div className="accountName">
+                <h4>Shincode</h4>
+                <span>#0410</span>
+              </div>
+            </div>
+            <div className="sidebarVoice">
+              <MicIcon />
+              <HeadphonesIcon />
+              <SettingsIcon />
+            </div>
           </div>
         </div>
       </div>
